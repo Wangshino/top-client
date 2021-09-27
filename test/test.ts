2b88475c7d4df5c06682a99e883b5246a9dfa8fd
wyzj1204@gmail.com <Candywz1>
2021-09-27 17:58:09 +0800

    [fix]新增了一些测试用例

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,5 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { format } from 'date-fns';
-import jsonBigInt = require('json-bigint');
 
 import TaobaoClient from '../src/index';
 import config from './config'; // Use your own key to run tests.
@@ -9,21 +7,13 @@ import config from './config'; // Use your own key to run tests.
 describe('Testing taobao top sdk', () => {
   const client = new TaobaoClient(config);
   let pictureId = '';
-  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
   test('Get time', async () => {
     const { data } = await client.invoke('taobao.time.get', {});
     delete data.request_id;
-    expect(data).toEqual({ time: format(new Date(), 'yyyy-MM-dd HH:mm:ss') });
+    let deltaTime = new Date().valueOf() - new Date(data.time).valueOf();
+    expect(deltaTime).toBeGreaterThanOrEqual(0);
   });
 
-  test('Get time twice', async () => {
-    await delay(2000);
-    const { data } = await client.invoke('taobao.time.get', {});
-    delete data.request_id;
-    expect(data).toEqual({ time: format(new Date(), 'yyyy-MM-dd HH:mm:ss') });
-  });
-
-  // 现在没有
   test('Upload picture', async () => {
     const buffer = fs.createReadStream(path.join(__dirname, '..', '..', 'test', 'ok.png'));
     const { data } = await client.invoke('taobao.picture.upload', {
@@ -31,8 +21,6 @@ describe('Testing taobao top sdk', () => {
       image_input_title: 'test.png',
       img: buffer,
     });
-    console.log(JSON.stringify(data));
-    console.log();
     pictureId = data.picture.picture_id;
     delete data.request_id;
     expect(data.picture).toHaveProperty('picture_id');
@@ -40,9 +28,17 @@ describe('Testing taobao top sdk', () => {
 
   test('taobao.picture.get', async () => {
     const { data } = await client.invoke('taobao.picture.get', {
-      title: 'test.png',
+      picture_id: pictureId,
     });
     delete data.request_id;
     expect(data.pictures).toHaveProperty('picture');
+  });
+
+  test('delete picture', async () => {
+    const { data } = await client.invoke('taobao.picture.delete', {
+      picture_ids: pictureId,
+    });
+    delete data.request_id;
+    expect(data).toHaveProperty('success', 'true');
   });
 });
